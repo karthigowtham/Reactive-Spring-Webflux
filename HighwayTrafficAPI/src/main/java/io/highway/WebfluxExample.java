f0a42f19e10b2b26b34e739cc4fa233769ad8d2b
karthigowtham <karthigowtham@LAPTOP-JCCHT4DJ>
2019-03-25 00:38:20 +0530

    Added new file

---
+++ HighwayTrafficAPI/src/main/java/io/highway/WebfluxExample.java
@@ -0,0 +1,25 @@
+package io.highway;
+
+public class WebfluxExample {
+
+	/*
+	 * public static void main(String[] args) throws InterruptedException {
+	 * 
+	 * Mono<Integer> mono = Mono.just(2); mono.map(i -> i *
+	 * 2).subscribe(System.out::println, null, () ->
+	 * System.out.println("MONO completed"));
+	 * 
+	 * // Flux<Integer> flux = Flux.just(1, 2, 3, 4, 5, 6, 7, 8, 9); // flux.map(i
+	 * -> i * 2).subscribe(System.out::println, null, () -> //
+	 * System.out.println("FLUX completed"));
+	 * 
+	 * // Flux.interval(Duration.ofSeconds(2)).map(i -> i * //
+	 * 2).subscribe(System.out::println, null, // () ->
+	 * System.out.println("FLUX completed"));
+	 * 
+	 * // Thread.sleep(10000);
+	 * 
+	 * }
+	 */
+
+}
