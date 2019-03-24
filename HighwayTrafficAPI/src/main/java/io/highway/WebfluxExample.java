d1259f8866214c9485d0fd7a96c6daf1911d84b3
karthigowtham <karthigowtham@LAPTOP-JCCHT4DJ>
2019-03-25 01:07:32 +0530

    Removed comment

--- HighwayTrafficAPI/src/main/java/io/highway/WebfluxExample.java
+++ HighwayTrafficAPI/src/main/java/io/highway/WebfluxExample.java
@@ -9,17 +9,16 @@ public class WebfluxExample {
 	 * 2).subscribe(System.out::println, null, () ->
 	 * System.out.println("MONO completed"));
 	 * 
-	 * // Flux<Integer> flux = Flux.just(1, 2, 3, 4, 5, 6, 7, 8, 9); // flux.map(i
-	 * -> i * 2).subscribe(System.out::println, null, () -> //
+	 * Flux<Integer> flux = Flux.just(1, 2, 3, 4, 5, 6, 7, 8, 9); flux.map(i -> i *
+	 * 2).subscribe(System.out::println, null, () ->
 	 * System.out.println("FLUX completed"));
 	 * 
-	 * // Flux.interval(Duration.ofSeconds(2)).map(i -> i * //
-	 * 2).subscribe(System.out::println, null, // () ->
+	 * Flux.interval(Duration.ofSeconds(2)).map(i -> i *
+	 * 2).subscribe(System.out::println, null, () ->
 	 * System.out.println("FLUX completed"));
 	 * 
-	 * // Thread.sleep(10000);
+	 * Thread.sleep(10000);
 	 * 
 	 * }
 	 */
-
 }
