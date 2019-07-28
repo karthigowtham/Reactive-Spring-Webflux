43929991afcdbc96dc60a6c8faa3576db3c3930f
karthigowtham <karthigowtham@LAPTOP-JCCHT4DJ>
2019-07-28 22:59:15 +0530

    Un commented

--- HighwayTrafficAPI/src/main/java/io/highway/WebfluxExample.java
+++ HighwayTrafficAPI/src/main/java/io/highway/WebfluxExample.java
@@ -1,24 +1,25 @@
 package io.highway;
 
+import java.time.Duration;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public class WebfluxExample {
 
-	/*
-	 * public static void main(String[] args) throws InterruptedException {
-	 * 
-	 * Mono<Integer> mono = Mono.just(2); mono.map(i -> i *
-	 * 2).subscribe(System.out::println, null, () ->
-	 * System.out.println("MONO completed"));
-	 * 
-	 * Flux<Integer> flux = Flux.just(1, 2, 3, 4, 5, 6, 7, 8, 9); flux.map(i -> i *
-	 * 2).subscribe(System.out::println, null, () ->
-	 * System.out.println("FLUX completed"));
-	 * 
-	 * Flux.interval(Duration.ofSeconds(2)).map(i -> i *
-	 * 2).subscribe(System.out::println, null, () ->
-	 * System.out.println("FLUX completed"));
-	 * 
-	 * Thread.sleep(10000);
-	 * 
-	 * }
-	 */
+	public static void main(String[] args) throws InterruptedException {
+
+		Mono<Integer> mono = Mono.just(2);
+		mono.map(i -> i * 2).subscribe(System.out::println, null, () -> System.out.println("MONO completed"));
+
+		Flux<Integer> flux = Flux.just(1, 2, 3, 4, 5, 6, 7, 8, 9);
+		flux.map(i -> i * 2).subscribe(System.out::println, null, () -> System.out.println("FLUX completed"));
+
+		Flux.interval(Duration.ofSeconds(2)).map(i -> i * 2).subscribe(System.out::println, null,
+				() -> System.out.println("FLUX completed"));
+
+		Thread.sleep(10000);
+
+	}
+
 }
